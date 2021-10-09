1b7bf5c4fe17962d974ee72bfa9470a6804675ee
alex <monkeymedic26@gmail.com>
2021-10-08 17:19:40 -0700

    removed boilerplate and namespace code

--- LearningCSharp/HelloWorld/Program.cs
+++ LearningCSharp/HelloWorld/Program.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("What is your Name?");
-            string str = Console.ReadLine();
-            Console.WriteLine("Why, hello there " + str);
-        }
-    }
-}
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your Name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
