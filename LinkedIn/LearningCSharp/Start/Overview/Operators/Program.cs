1bc6bfead2e83b59d6b0a268c24b510a6255f82d
monkeymedic26 <monkeymedic26@gmail.com>
2022-11-01 22:25:42 -0600

    Finish Basic Math and Shorthand sections

--- LinkedIn/LearningCSharp/Start/Overview/Operators/Program.cs
+++ LinkedIn/LearningCSharp/Start/Overview/Operators/Program.cs
@@ -12,13 +12,21 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x/y) * x);
+            Console.WriteLine(a + b);
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
 
             // TODO: Operators can be shorthand: a = a + b
+            a += b;
+            Console.WriteLine(a);
 
 
             // TODO: Logical operators &&, ||
@@ -29,7 +37,7 @@ namespace Operators
             // string str = null;
             // TODO: the ?? operator uses left operand if not null, or right one if it is
 
-            
+
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
