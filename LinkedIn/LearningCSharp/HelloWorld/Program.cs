d2c63d1cf998387472d3a472a0f7db6252535f2a
monkeymedic26 <monkeymedic26@gmail.com>
2022-11-01 22:05:44 -0600

    Finish Intro

--- LinkedIn/LearningCSharp/HelloWorld/Program.cs
+++ LinkedIn/LearningCSharp/HelloWorld/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("What is your name?");
+    string str = Console.ReadLine();
+    Console.WriteLine("Why, hello there " + str);
